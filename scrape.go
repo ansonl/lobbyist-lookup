8a889f7473eb1ccf28339d36932779be5dbe55c2
ansonl <ansonl@users.noreply.github.com>
2014-06-13 21:35:01 +0000

    unzip

--- scrape.go
+++ scrape.go
@@ -26,49 +26,37 @@ var tmpName = "tmp"
 var fileExt = ".zip"
 var link = "http://disclosures.house.gov/ld/LDDownload.aspx?KeepThis=true"
 
-func unzipFile(f *zip.File, dest string) error {
-    rc, err := f.Open()
-    if err != nil {
-        return err
-    }
-    defer rc.Close()
-
-    path := filepath.Join(dest, f.Name)
-    if f.FileInfo().IsDir() {
-        err := os.MkdirAll(path, f.Mode())
-        if err != nil {
-            return err
-        }
-    } else {
-        f, err := os.OpenFile(
-            path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-        if err != nil {
-            return err
-        }
-        defer f.Close()
-
-        _, err = io.Copy(f, rc)
-        if err != nil {
-            return err
-        }
-    }
-    return nil
-}
-
 func Unzip(src, dest string) error {
     r, err := zip.OpenReader(src)
     if err != nil {
         return err
     }
-    defer r.Close()
 
     for _, f := range r.File {
-        err := unzipFile(f, dest)
+        rc, err := f.Open()
         if err != nil {
-            return err
+            panic(err)
         }
-    }
 
+        path := filepath.Join(dest, f.Name)
+        if f.FileInfo().IsDir() {
+            os.MkdirAll(path, f.Mode())
+        } else {
+            f, err := os.OpenFile(
+                path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+            if err != nil {
+                panic(err)
+            }
+
+            _, err = io.Copy(f, rc)
+            if err != nil {
+                panic(err)
+            }
+            f.Close()
+        }
+        rc.Close()
+    }
+    r.Close()
     return nil
 }
 
