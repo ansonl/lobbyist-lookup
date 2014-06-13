8acb2fb79503004877025e901bb00883c7fbf81c
ansonl <ansonl@users.noreply.github.com>
2014-06-13 21:36:57 +0000

    unzip

--- scrape.go
+++ scrape.go
@@ -31,13 +31,15 @@ func Unzip(src, dest string) error {
     if err != nil {
         return err
     }
-
+    defer r.Close()
+ 
     for _, f := range r.File {
         rc, err := f.Open()
         if err != nil {
-            panic(err)
+            return err
         }
-
+        defer rc.Close()
+ 
         path := filepath.Join(dest, f.Name)
         if f.FileInfo().IsDir() {
             os.MkdirAll(path, f.Mode())
@@ -45,18 +47,17 @@ func Unzip(src, dest string) error {
             f, err := os.OpenFile(
                 path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
             if err != nil {
-                panic(err)
+                return err
             }
-
+            defer f.Close()
+ 
             _, err = io.Copy(f, rc)
             if err != nil {
-                panic(err)
+                return err
             }
-            f.Close()
         }
-        rc.Close()
     }
-    r.Close()
+ 
     return nil
 }
 
