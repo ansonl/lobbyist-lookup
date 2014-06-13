93b90301a0f9a8b295b9b6eebc0d29f7c1bdae56
ansonl <ansonl@users.noreply.github.com>
2014-06-13 03:21:20 +0000

    initial commit

---
+++ api.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"encoding/xml"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"path/filepath"
+	"net/http"
+	//"html/template"
+	"time"
+	"os"
+	"strconv"
+)
+
+var directory = "filings"
+
+type Lobbyist struct {
+	FirstName string `xml:"lobbyistFirstName"`
+	LastName string `xml:"lobbyistLastName"`
+}
+
+type Registration struct {
+	OrganizationName string `xml:"organizationName"`
+	ClientName string `xml:"clientName"`
+	SenateID string `xml:"senateID"`
+	HouseID string `xml:"houseID"`
+	//ReportYear string `xml:"reportYear"`
+	//ReportType string `xml:"reportType"`
+	//Lobbyist []Lobbyist `xml:"alis>ali_info>lobbyists>lobbyist"` //apparently house changed their xml format on 6/10??
+	Lobbyist []Lobbyist `xml:"lobbyists>lobbyist"`
+}
+
+var rArray []Registration
+
+//return a json formatted string for a day struct
+func (reg Registration) JSONString() []byte {
+	b, err := json.Marshal(reg)
+	if (err != nil) {
+		panic(err)
+	}
+	return b
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    data, err := ioutil.ReadFile("./pages/api.html")
+    if (err != nil) {
+		panic(err)
+	}
+    fmt.Fprintf(w, string(data))
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+
+    	r.ParseForm()
+    	fmt.Println(r.Form)
+    
+    	//bypass same origin policy
+    	w.Header().Set("Access-Control-Allow-Origin", "*")
+    
+    	//setup return string
+    	returnString := "{" + `"` + "array" + `"` + ":" + "[ ";
+    
+    	//firstName := r.Form["first"]
+    	lastName := r.Form["last"]
+    	companyName := r.Form["company"]
+    
+        limit := 10
+        count := 0
+    
+    	for _, i := range rArray {
+    	    if (count < limit) {
+        		if (companyName != nil) {
+        			for _, k := range companyName {
+        				if (strings.Contains(strings.ToLower(i.OrganizationName), k)) {
+        					if (lastName != nil) {
+        						for _, j := range i.Lobbyist {
+        							if (j.LastName != "") {
+        								for _, l := range lastName {
+        									if (strings.Contains(strings.ToLower(j.LastName), l)) {
+        										returnString += string(i.JSONString()) + ","
+        										count++
+        									}
+        								}
+        							}
+        						}
+        					} else {
+        						returnString += string(i.JSONString()) + ","
+        						count++
+        					}
+        				}
+        			}
+        		} else {
+        		    if (lastName != nil) {
+            			for _, j := range i.Lobbyist {
+            				if (j.LastName != "") {
+            					for _, l := range lastName {
+            						if (strings.Contains(strings.ToLower(j.LastName), l)) {
+            							returnString += string(i.JSONString()) + ","
+            							count++
+            						}
+            					}
+            				}
+            			}
+        		    }
+        		}
+    	    }
+    	}
+    
+    	returnString = returnString[:len(returnString) - 1]
+    	returnString += "]" + "}"
+    	fmt.Fprintf(w, returnString)
+}
+
+func server() {
+	http.HandleFunc("/api/", apiHandler)
+	http.HandleFunc("/", handler)
+	//http.ListenAndServe(":8080", nil)
+    
+    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil) 
+    if err != nil {
+      panic(err)
+    }  
+
+    fmt.Println("listening on port " + os.Getenv("PORT"))
+}
+
+func readDirectory(recordDir string) {
+    files, err := ioutil.ReadDir("./" + recordDir + "/")
+	if (err != nil) {
+		panic(err)
+	}
+
+    fmt.Println("Reading " + strconv.Itoa(len(files)) + " files from " + recordDir + "...")
+
+	rArray = make([]Registration, len(files))
+
+	a := 0 //counter for number of files successfully read
+
+	for _, f := range files {
+		
+		func() {
+            data, err := ioutil.ReadFile("./" + recordDir + "/" + f.Name())
+            if err != nil {
+    			fmt.Println("error reading %v", err)
+    			return
+    		} else {
+    	        if (strings.Contains(filepath.Ext(f.Name()), "xml")) {
+    
+    	        	//unmarshal data and put into struct array
+    				err = xml.Unmarshal([]byte(data), &rArray[a])
+    				if err != nil {
+    					fmt.Println("error decoding %v: %v",f.Name(), err)
+    					return
+    				}
+    
+    				a++ //increment number of files successfully parsed
+    			}
+    		}
+		}()
+		
+		if (a % 1000 == 0) {
+		    fmt.Println(strconv.Itoa(a) + " files read");
+		}
+    }
+
+    fmt.Println("Successfully read " , a , " / " , len(files) , " files.")
+}
+
+func main() {
+    scrape()
+    
+	readDirectory(directory)
+    
+    //getTokens()
+
+    server()
+
+    t := time.NewTicker(60 * time.Second)
+
+	for now := range t.C {
+		now = now
+	}
+
+	fmt.Println("server end")
+}
---
+++ scrape.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "code.google.com/p/go.net/html"
+    "strings"
+    "net/url"
+    "io/ioutil"
+    "time"
+    "strconv"
+    "io"
+    "os"
+    "archive/zip"
+    "path/filepath"
+    "sync"
+)
+
+type Tokens struct {
+    ViewState, EventValidation string;
+    FileNames [5]string; //max of four quarters and one new registration option a year
+}
+
+var savePath = "./filings/"
+var tmpName = "tmp"
+var fileExt = ".zip"
+var link = "http://disclosures.house.gov/ld/LDDownload.aspx?KeepThis=true"
+
+func Unzip(src, dest string) error {
+    r, err := zip.OpenReader(src)
+    if err != nil {
+        return err
+    }
+    defer r.Close()
+ 
+    for _, f := range r.File {
+        rc, err := f.Open()
+        if err != nil {
+            return err
+        }
+        defer rc.Close()
+ 
+        path := filepath.Join(dest, f.Name)
+        if f.FileInfo().IsDir() {
+            os.MkdirAll(path, f.Mode())
+        } else {
+        	func() {
+	            f, err := os.OpenFile(
+	                path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	            if err != nil {
+	                panic(err)
+	            }
+	            defer f.Close()
+	 
+	            _, err = io.Copy(f, rc)
+	            if err != nil {
+	                panic(err)
+	            }
+        	}()
+        }
+    }
+ 
+    return nil
+}
+
+func download(t Tokens, i int, wg *sync.WaitGroup) {
+    //set POST data
+    v := url.Values{}
+	v.Set("__VIEWSTATE", t.ViewState)
+	v.Set("__EVENTVALIDATION", t.EventValidation)
+	v.Set("selFilesXML", t.FileNames[i])
+	v.Set("btnDownloadXML", "Download")
+    
+    fmt.Println("Downloading " + t.FileNames[i] + " to " + savePath + tmpName + fileExt + "...")
+    
+    //pres, err := http.PostForm("http://vm-2.ansonl.koding.kd.io/php.php", v)
+    pres, err := http.PostForm(link, v)
+    
+    robots, err := ioutil.ReadAll(pres.Body)
+	pres.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(savePath + tmpName + strconv.Itoa(i) + fileExt, robots, 0644)
+		if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println("Downloaded " + t.FileNames[i] + " to " + savePath + tmpName + strconv.Itoa(i) + fileExt)
+	
+	//Unzipping
+	fmt.Println("Unzipping " + savePath + tmpName + strconv.Itoa(i) + fileExt + " to " + savePath + "...")
+	
+	err = Unzip(savePath + tmpName + strconv.Itoa(i) + fileExt, savePath)
+    if err != nil {
+        panic(err)
+    }
+    
+    fmt.Println("Unzipped " + savePath + tmpName + strconv.Itoa(i) + fileExt + " to " + savePath)
+    
+    //Waitgroup done
+    wg.Done()
+}
+
+func scrape() {
+    fmt.Println("Sending GET request to " + link + "...");
+    
+    res, err := http.Get(link)
+    
+    if (err != nil) {
+        panic(err)
+    }
+    
+    doc, err := html.Parse(res.Body)
+    
+    token := Tokens{}
+    tokenFileNamesCount := 0;
+    
+    //documentation https://godoc.org/code.google.com/p/go.net/html#Attribute
+    var f func(*html.Node)
+    f = func(n *html.Node) {
+        //'n' is a node representing ONE object on the page
+        //fmt.Println(n.Attr)
+        if n.Type == html.ElementNode {
+            for _, a := range n.Attr {
+                
+                //fmt.Println(a)
+                //fmt.Println(a.Namespace, a.Key, a.Val)
+                if (len(a.Val) > 3 && strings.Contains(a.Val[0:4], strconv.Itoa(time.Now().Year())) && tokenFileNamesCount < 5) { //check first four characters of attr value to get year
+                    token.FileNames[tokenFileNamesCount] = a.Val
+                    tokenFileNamesCount++
+                }
+                if (a.Val == "__VIEWSTATE") {
+                    //fmt.Println(n.Attr[2].Key, n.Attr[2].Val)
+                    for _, b := range n.Attr { //loop through this same object again, looking for the attribute in the slice which has key of "value"
+                        if (b.Key == "value") {
+                            token.ViewState = b.Val
+                        }
+                    }
+                }
+                if (a.Val == "__EVENTVALIDATION") {
+                    for _, b := range n.Attr { //loop through this same object again, looking for the attribute in the slice which has key of "value"
+                        if (b.Key == "value") {
+                            token.EventValidation = b.Val
+                        }
+                    }
+                }
+            }
+        }
+        for c := n.FirstChild; c != nil; c = c.NextSibling { //advance node
+            f(c)
+        }
+    }
+    f(doc)
+    
+    //Download each file in a thread
+    
+    
+    if _, err := os.Stat(savePath); err != nil {
+        if os.IsNotExist(err) {
+            err = os.Mkdir(savePath, 0777);
+            if err != nil {
+                panic(err)
+            }
+        } else {
+            panic(err)
+        }
+    }
+    
+    fmt.Println(savePath + " directory made")
+    
+    var wg sync.WaitGroup
+    for fileNumber, file := range token.FileNames {
+        if file != "" {
+            wg.Add(1)
+            go download(token, fileNumber, &wg)            
+        }
+    }
+    wg.Wait()
+    fmt.Println("All files downloaded.")
+}
