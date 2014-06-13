21c94cf9451138c59019b0687ee8618ce4bc435d
ansonl <ansonl@users.noreply.github.com>
2014-06-13 03:29:14 +0000

    unzip

--- scrape.go
+++ scrape.go
@@ -34,30 +34,32 @@ func Unzip(src, dest string) error {
     defer r.Close()
  
     for _, f := range r.File {
-        rc, err := f.Open()
-        if err != nil {
-            return err
-        }
-        defer rc.Close()
- 
-        path := filepath.Join(dest, f.Name)
-        if f.FileInfo().IsDir() {
-            os.MkdirAll(path, f.Mode())
-        } else {
-        	func() {
-	            f, err := os.OpenFile(
-	                path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-	            if err != nil {
-	                panic(err)
-	            }
-	            defer f.Close()
+    	func() {
+	        rc, err := f.Open()
+	        if err != nil {
+	            panic(err)
+	        }
+	        defer rc.Close()
 	 
-	            _, err = io.Copy(f, rc)
-	            if err != nil {
-	                panic(err)
-	            }
-        	}()
-        }
+	        path := filepath.Join(dest, f.Name)
+	        if f.FileInfo().IsDir() {
+	            os.MkdirAll(path, f.Mode())
+	        } else {
+	        	func() {
+		            f, err := os.OpenFile(
+		                path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		            if err != nil {
+		                panic(err)
+		            }
+		            defer f.Close()
+		 
+		            _, err = io.Copy(f, rc)
+		            if err != nil {
+		                panic(err)
+		            }
+	        	}()
+	        }
+    	}()
     }
  
     return nil
