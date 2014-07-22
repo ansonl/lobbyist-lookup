771015ce42eb0d35168ecd2a3aa1826f56ad939d
ansonl <ansonl@users.noreply.github.com>
2014-07-22 19:08:43 +0000

    moved files

---
+++ form.js
@@ -0,0 +1,101 @@
+var queryForm;
+var lookupButton;
+
+var leftDiv;
+var rightDiv;
+
+var mainContentDiv;
+var formDiv;
+var loadingDiv;
+var resultDiv;
+
+var loadingText;
+
+var lookupSuccess = function (data) {
+	loadingText.fadeOut(100, function() {
+		loadingText.text('Parsing filings locally');
+		loadingText.fadeIn(100);
+	});
+
+	//array of filing results returned by parseData() located in filing.js
+	var filings = parseData(JSON.parse(data));
+	resultDiv.html(createTable(filings));
+
+	loadingDiv.css('display', 'none');
+	resultDiv.css('display', 'inline');
+
+	leftDiv.attr('class', 'col-lg-1');
+	rightDiv.attr('class', 'col-lg-1');
+	mainContentDiv.attr('class', 'col-lg-10');
+};
+
+var lookupError = function (data) {
+	loadingDiv.css('display', 'none');
+	resultDiv.css('display', 'inline');
+};
+
+var lookupComplete = function (data) {
+	loadingDiv.css('display', 'none');
+};
+
+var formEditted = function (data) {
+	if (data.target.value.length > 0)
+		lookupButton.prop('disabled', false);
+}
+
+var formChanged = function (data) {
+	//lowercase input for matching
+	data.target.value = data.target.value.toLowerCase();
+}
+
+$(document).ready(function() {
+	$('body').css('display', 'none');
+	$('body').fadeIn(100);
+
+	queryForm = $('#queryForm');
+	lookupButton = $('#lookupButton');
+
+	lookupButton.prop('disabled', true);
+
+	leftDiv = $('#leftDiv');
+	rightDiv = $('#rightDiv');
+
+	mainContentDiv = $('#mainContentDiv');
+	formDiv = $('#formDiv');
+	loadingDiv = $('#loadingDiv');
+	resultDiv = $('#resultDiv');
+
+	loadingText = $('#loadingText');
+
+	$('.form-control').on('input', formEditted);
+	$('.form-control').change(formChanged);
+
+	queryForm.submit(function() {
+
+		if ($('#surnameInput').val().length > 0 || $('#organizationInput').val().length > 0 || $('#clientInput').val().length > 0) {
+
+			loadingText.text('Looking up filings');
+			formDiv.fadeOut(100, function() {
+				//Check if ajax callback has already been called
+				if (resultDiv.css('display') == 'none')
+					loadingDiv.fadeIn(100);
+			});
+			
+
+			var url = 'http://lobbyist.herokuapp.com/api/';
+			$.ajax({
+				type: 'GET',
+				url: url,
+				data: queryForm.serialize(),
+				success: lookupSuccess,
+				error: lookupError,
+				complete: lookupComplete
+			});
+		} else {
+			console.log('not pass')
+		}
+		return false;
+	});
+})
+
+
---
+++ table.js
@@ -0,0 +1,89 @@
+function formatLobbyist(lobbyist) {
+	var tmp = '';
+
+	if (lobbyist.length == 0) {
+		tmp += '<span class="noFilingsRow">' + 'No lobbyists on file' + '</span>';
+	} else {
+		lobbyist.forEach(function(entry) {
+			if ($('#surnameInput').val() != '' && entry.lastName.toLowerCase().indexOf($('#surnameInput').val().toLowerCase()) > -1) {
+				tmp += '<span class="highlightedMatch">' + entry.firstName.toUpperCase() + ' ' + entry.lastName.toUpperCase() + '</span><br>';
+			} else {
+				tmp += entry.firstName.toUpperCase() + ' ' + entry.lastName.toUpperCase() + '<br>';
+			}
+		});
+	}
+
+	return tmp;
+}
+
+function formatGenericField(field) {
+	var tmp = '';
+	if (field == '') {
+		tmp += '<span class="noFilingsRow">' + 'N/A' + '</span>';
+	} else {
+		tmp += field.toUpperCase();
+	}
+
+	return tmp;
+}
+
+function formatFiling(filing, i) {
+	var tmp = '';
+
+	tmp += '<tr>';
+
+	tmp += '<td>' + i + '</td>';
+	tmp += '<td>' + formatGenericField(filing.organization) + '</td>';
+	tmp += '<td>' + formatGenericField(filing.client) + '</td>';
+	tmp += '<td>' + formatGenericField(filing.houseId) + '</td>';
+	tmp += '<td>' + formatGenericField(filing.senateId) + '</td>';
+	tmp += '<td class="lobbyistColumn">' + formatLobbyist(filing.lobbyistArray) + '</td>';
+
+	tmp += '</tr>';
+
+	return tmp;
+}
+
+function formatTableHeader() {
+	return '<thead><tr><th>#</th><th>Filing Organization</th><th>Organization Client</th><th>House ID</th><th>Senate ID</th><th>Lobbyists</th></thead>';
+}
+
+function formatNoResults() {
+	var tmp = '';
+
+	tmp += '<tr>';
+
+	tmp += '<td colspan="6" class="noFilingsRow">' + 'No filings matched' + '</td>';
+
+	tmp += '</tr>';
+
+	return tmp;
+}
+
+function createTable(filingArray) {
+	var tmp = '';
+
+	tmp += '<h5>';
+	if (filingArray.length > 9) {
+		tmp += 'First ' + filingArray.length + ' filings shown. There may be more matching filings.';
+	} else {
+		tmp += filingArray.length + ' filings found.';
+	}
+	tmp += '</h5>';
+
+	tmp += '<table class="table table-bordered table-hover table-condensed table-responsive"><colgroup><col width="auto"/><col width="200em"/><col width="200em"/><col width="auto"/><col width="auto"/><col width="auto"/></colgroup>' + formatTableHeader() + '<tbody>';
+
+	if (filingArray.length == 0) {
+		tmp += formatNoResults();
+	} else {
+		var i = 1;
+		filingArray.forEach(function(entry) {
+			tmp += formatFiling(entry, i);
+			i++;
+		});
+	}
+
+	tmp += '</tbody></table>';
+
+	return tmp;
+}
