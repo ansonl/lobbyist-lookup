9ff53698faf32ea32a334a4ddc28dab55a7c1e4e
Anson Liu <ansonl@users.noreply.github.com>
2014-08-09 17:17:54 -0700

    script update

--- goinstall.sh
+++ goinstall.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
-cd ~
 
+#setup Go lang
+cd ~
 wget http://golang.org/dl/go1.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.3.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
 mkdir $HOME/go
+
+#set envvars
+export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-mkdir -p $GOPATH/src/github.com/ansonl
 export PORT=80
 
-sudo apt-get install git
-sudo apt-get install mercurial
-sudo apt-get install htop
+mkdir -p $GOPATH/src/github.com/ansonl
+
+#install git, hg, htop
+sudo apt-get install git mercurial htop
+
+#clone lobbyist-lookup
+cd go/src/github.com/ansonl
+git clone https://github.com/ansonl/lobbyist-lookup
+
+#build and run
+cd lobbyist-lookup
+go get
+
+sudo setcap 'cap_net_bind_service=+ep' /home/$USER/go/bin/lobbyist-lookup
+
+nohup lobbyist-lookup &
